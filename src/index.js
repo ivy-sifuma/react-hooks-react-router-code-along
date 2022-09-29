83dc235ddae88c40c4aac3909ac1f715c600f2c4
Ivy Sifuma <ivy.sifuma@student.moringaschool.com>
2022-09-29 15:47:14 +0300

    Soils

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
+/* Add NavLink to import */
+import {BrowserRouter, Routes, Route } from "react-router-dom";
+
+/* Add basic styling for NavLinks */
+const linkStyles = {
+  display: "inline-block",
+  width: "50px",
+  padding: "12px",
+  margin: "0 6px 6px",
+  background: "blue",
+  textDecoration: "none",
+  color: "white",
+};
 
 function Home() {
   return (
@@ -8,6 +21,72 @@ function Home() {
     </div>
   );
 }
+
+function NavBar() {
+  return (
+    <div>
+      <h1>Navigation</h1>
+     </div>
+
+  );
+}
+
+ReactDOM.render(
+  <BrowserRouter>
+    <NavBar />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
+
+function About() {
+  return (
+    <div>
+      <h1>This is my about component!</h1>
+    </div>
+  );
+}
+
+function Login() {
+  return (
+    <div>
+      <h1>Login</h1>
+      <form>
+        <div>
+          <input type="text" name="username" placeholder="Username" />
+        </div>
+        <div>
+          <input type="password" name="password" placeholder="Password" />
+        </div>
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
+}
+
+ReactDOM.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
+
+ReactDOM.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
 
 function App() {
   return <Home />;
